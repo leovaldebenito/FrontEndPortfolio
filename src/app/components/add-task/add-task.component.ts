2a79346308cdfd331afe4f30efe602b1ade3c21c
leovaldebenito <leonardovaldebenito21@gmail.com>
2023-03-26 11:07:05 -0300

    about y footer

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/service/ui.service';
 import { Task } from '../../Task'
 
 @Component({
@@ -11,6 +13,14 @@ export class AddTaskComponent {
   text:string="";
   day:string="";
   reminder:boolean=false;
+  showAddTask:boolean=false;
+  subscription?: Subscription;
+
+  constructor(
+    private uiService: UiService
+  ) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   onSubmit(){
     if(this.text.length === 0){
@@ -19,6 +29,7 @@ export class AddTaskComponent {
     }
     const {text,day,reminder} = this
     const newTask={text,day,reminder}
+
     this.onAddTask.emit(newTask);
   }
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -8,7 +11,22 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
 
   title:string = 'Portfolio Leonardo Valdebenito';
+
+  showAddTask:boolean = true;
+  subscription?: Subscription;
+
+  constructor(
+    private UiService: UiService,
+    private router: Router
+  ) {
+    this.subscription = this.UiService.onToggle().subscribe(value => this.showAddTask = value )
+  }
+
   toggleAddTask(){
     console.log("toggleAddTask!");
+  }
+
+  hasRoute(route:string){
+    return this.router.url === route
   }
 }
